4e78ccb93c6d30ba9887ddef6eec451beef7d0a6
Bao Nguyen <chibaobguyen5585@gmail.com>
2021-11-03 23:53:30 +0700

    Tao store vuex de luu tru du lieu/trang thai dang nhap cua nguoi dung

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,36 @@
   <div id="app">
     <nav class="navbar navbar-expand navbar-dark bg-dark">
       <a href="/" class="navbar-brand">Ứng dụng Quản lý danh bạ</a>
+
       <div class="mr-auto navbar-nav">
         <li class="nav-item">
           <router-link to="/contactbook" class="nav-link">
             Danh bạ <i class="fas fa-address-book"></i>
           </router-link>
+        </li>
+      </div>
+
+      <div v-if="!currentUser" class="navbar-nav ml-auto">
+        <li class="nav-item">
+          <router-link to="/register" class="nav-link">
+            Đăng ký
+          </router-link>
+        </li>
+        <li class="nav-item">
+          <router-link to="/login" class="nav-link">
+            Đăng nhập
+        </router-link>
+        </li>
+      </div>
+
+      <div v-if="currentUser" class="navbar-nav ml-auto">
+        <li class="nav-item">
+          <router-link to="/profile" class="nav-link">
+            {{ currentUser.username }}
+          </router-link>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" @click.prevent="logout"> Đăng xuất </a>
         </li>
       </div>
     </nav>
@@ -18,8 +43,38 @@
 </template>
 
 <script>
+import { mapGetters, mapMutations } from "vuex";
+
 export default {
   name: "App",
+  computed: {
+    ...mapGetters({
+      currentUser: "loggedInUser",
+    }),
+  },
+  methods: {
+    ...mapMutations([
+      "initAuthState" // map this.initAuthState() to this.$store.commit("initAuthState")
+    ]),
+
+    logout() {
+      this.$store.commit("logout");
+      this.$router.push("login");
+    },
+  },
+  mounted() {
+    this.initAuthState();
+  }
+  /*data() {
+    return {
+      currentUser: null
+    };
+  },
+  methods: {
+    logout(){
+      console.log("logout");
+    }
+  }*/
 };
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,14 +1,21 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import store from './store';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import router from "./router";
 import { handle } from "./common/promise";
 
-const app=createApp(App);
-app.use(router);
-app.mixin({methods: {handle}});
-app.mount("#app");
+createApp(App)
+    .mixin({
+        methods: {
+        handle,
+        },
+    })
+    .use(store)
+    .use(router)
+    .mount("#app");
+
 
 
 
